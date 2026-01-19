aefdf1eb80b24ec0775973766d88d084fd8289b3
Elizabeth <chrisdaron2@gmail.com>
2026-01-19 20:05:21 +0100

    Add working Terraform infrastructure with EC2, VPC, subnet, SG

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,3 +1,15 @@
+
+resource "aws_vpc" "app_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "app-vpc"
+  }
+}
+
+
 ############################
 # Security Group
 ############################
@@ -32,4 +44,47 @@ resource "aws_security_group" "app_sg" {
   }
 }
 
+
+############################
+# Public Subnet
+############################
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.app_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-1"
+  }
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["amazon"]
+}
+
+
+
+resource "aws_instance" "app_ec2" {
+  ami                         = "ami-015f3aa67b494b27e"
+  instance_type               = "t3.micro"
+  subnet_id                   = aws_subnet.public.id
+  vpc_security_group_ids      = [aws_security_group.app_sg.id]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "app-ec2"
+  }
+}
 
--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -1,9 +1,4 @@
 output "ec2_public_ip" {
-  value = aws_instance.app_server.public_ip
+  value = aws_instance.app_ec2.public_ip
 }
-
-output "rds_endpoint" {
-  value = aws_db_instance.app_db.endpoint
-}
-
 
