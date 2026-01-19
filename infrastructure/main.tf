d63bbca137223fc6ca758672daf8323e9377f6b2
Elizabeth <chrisdaron2@gmail.com>
2026-01-19 19:02:40 +0100

    Add Terraform infrastructure: VPC, public subnets, ALB, EC2, security groups

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -4,6 +4,7 @@
 resource "aws_security_group" "app_sg" {
   name        = "app-security-group"
   description = "Allow SSH and HTTP"
+  vpc_id      = aws_vpc.app_vpc.id
 
   ingress {
     from_port   = 22
@@ -25,39 +26,9 @@ resource "aws_security_group" "app_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-}
-
-############################
-# EC2 Instance
-############################
-resource "aws_instance" "app_server" {
-  ami           = "ami-0c02fb55956c7d316"
-  instance_type = var.instance_type
-
-  vpc_security_group_ids = [
-    aws_security_group.app_sg.id
-  ]
 
   tags = {
-    Name = "app-ec2"
-  }
-}
-
-############################
-# RDS Database
-############################
-resource "aws_db_instance" "app_db" {
-  allocated_storage   = 20
-  engine              = "mysql"
-  engine_version      = "8.0"
-  instance_class      = "db.t3.micro"
-  db_name             = "appdb"
-  username            = var.db_username
-  password            = var.db_password
-  skip_final_snapshot = true
-
-  tags = {
-    Name = "app-rds"
+    Name = "app-sg"
   }
 }
 
