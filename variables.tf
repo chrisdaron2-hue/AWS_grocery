5f111853c39a0e4ca210b1d0363b88f3ef0e350e
Elizabeth <chrisdaron2@gmail.com>
2026-01-30 09:45:15 +0100

    Terraform infrastructure: updated main.tf, removed old resource files, updated variables

--- variables.tf
+++ variables.tf
@@ -1,57 +1,36 @@
-# variables.tf
-
-variable "instance_type" {
-  description = "EC2 instance type"
-  type        = string
-  default     = "t3.micro"
-}
-
-variable "db_username" {
-  description = "RDS database username"
-  type        = string
-}
-
-variable "db_password" {
-  description = "RDS database password"
-  type        = string
-  sensitive   = true
-}
-
-variable "vpc_a_cidr" {
-  description = "CIDR block for VPC A"
-  type        = string
-  default     = "10.0.0.0/16"
-}
-
-variable "vpc_b_cidr" {
-  description = "CIDR block for VPC B"
-  type        = string
-  default     = "10.1.0.0/16"
-}
-
-variable "subnet_a_cidr" {
-  description = "CIDR block for Subnet A"
-  type        = string
-  default     = "10.0.1.0/24"
-}
-
-variable "subnet_b_cidr" {
-  description = "CIDR block for Subnet B"
-  type        = string
-  default     = "10.0.2.0/24"
-}
 variable "region" {
-  description = "AWS region to deploy resources in"
+  description = "The AWS region to deploy resources in"
   type        = string
-  default     = "us-east-1"
+  default     = "eu-central-1"  # ✅ literal value, not var.region
 }
+
+variable "db_engine" {
+  description = "Database engine"
+  type        = string
+  default     = "postgres"
+}
+
 variable "db_name" {
   description = "The name of the RDS database"
   type        = string
-  default     = "mydatabase" # or you can remove default to make it required
+  default     = "defaultdb"
 }
 
+variable "db_username" {
+  description = "Database username"
+  type        = string
+  default     = "postgres"
+}
 
+variable "db_password" {
+  description = "Database password"
+  type        = string
+  default     = "Password123!"
+  sensitive   = true
+}
 
-
-
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+  sensitive   = true
+}
